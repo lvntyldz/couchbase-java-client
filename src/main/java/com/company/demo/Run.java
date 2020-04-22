1b5bee8c0b309350773a354b0a72a40c6207a50d
levent yıldız <lewenty@gmail.com>
2020-04-22 18:33:06 +0300

    Add update and delete

--- src/main/java/com/company/demo/Run.java
+++ src/main/java/com/company/demo/Run.java
@@ -1,11 +1,17 @@
 package com.company.demo;
 
+import com.couchbase.client.core.deps.com.fasterxml.jackson.databind.ObjectMapper;
+import com.couchbase.client.core.error.DocumentNotFoundException;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.json.JsonArray;
 import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.client.java.kv.GetResult;
 import com.couchbase.client.java.kv.MutationResult;
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class Run {
 
@@ -27,6 +33,34 @@ public class Run {
         Employee employee2 = new Employee("Veli","VELİOĞLU",30,job2);
         MutationResult employe2Res = collection.upsert("employe2", employee2);
         System.out.println("Employee2 Result : "+employe2Res);
+
+        System.out.println("\nGetting employe1 from couchbase...");
+        GetResult result = collection.get("employe1");
+        Employee employeeRes = result.contentAs(Employee.class);
+        System.out.println("Employee1 : "+employeeRes);
+
+        System.out.println("\nUpdating employe1...");
+        employeeRes.setLastname("HASANOĞLU");
+        employeeRes.setFirstname("Hasan");
+        collection.upsert("employe1",employeeRes);
+
+        GetResult updatedResult = collection.get("employe1");
+        Employee updatedEmployeeRes = updatedResult.contentAs(Employee.class);
+        System.out.println("Employee1 : "+updatedEmployeeRes);
+
+
+        System.out.println("\nDeleting employe2...");
+        collection.remove("employe2");
+
+        try{
+            GetResult deletedResult = collection.get("employe2");
+            Employee deletedEmployeeRes = deletedResult.contentAs(Employee.class);
+            System.out.println("Employe2 : "+deletedEmployeeRes);
+        }catch (DocumentNotFoundException e){
+            System.out.println("Employee2 deleted.");
+        }
+
+
     }
 
 }
